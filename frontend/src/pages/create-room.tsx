186bd7b62f8fff9948db6255ec03188ac760a3eb
Yan Silva <yandsilva94@gmail.com>
2025-07-11 12:18:48 -0300

    feat: create add create-room-form, room-list, question-form, question-item

--- frontend/src/pages/create-room.tsx
+++ frontend/src/pages/create-room.tsx
@@ -1,37 +1,16 @@
-import { useQuery } from "@tanstack/react-query";
-import { Link } from "react-router-dom";
-
-type GetRoomsAPIResponse = Array<{
-  id: string;
-  name: string;
-}>;
+import { CreateRoomForm } from "@/components/create-room-form";
+import { RoomList } from "@/components/room-list";
 
 export function CreateRoom() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-rooms"],
-    queryFn: async () => {
-      const response = await fetch("http://localhost:3333/rooms");
-      const result: GetRoomsAPIResponse = await response.json();
-
-      return result;
-    },
-  });
   return (
-    <div>
-      <h1>Create Room</h1>
+    <div className="min-h-screen px-4 py-8">
+      <div className="mx-auto max-w-4xl">
+        <div className="grid gap-8 grid-cols-2 items-start">
+          <CreateRoomForm />
 
-      {isLoading && <p>Loading...</p>}
-      <div className="flex flex-col gap-1">
-        {data?.map((room) => {
-          return (
-            <Link key={room.id} to={`/room/${room.id}`}>
-              {room.name}
-            </Link>
-          );
-        })}
+          <RoomList />
+        </div>
       </div>
-
-      <Link to={"/room"}>Acessar Pagina</Link>
     </div>
   );
 }
